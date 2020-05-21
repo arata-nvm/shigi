040d5af653381e6794b4284ff53f5ed6c3ed56d0
arata <arata04962@gmail.com>
2020-05-22 00:15:13 +0900

    [add] Structures for DOM

---
+++ src/dom.rs
@@ -0,0 +1,39 @@
+use std::collections::HashMap;
+
+pub type AttrMap = HashMap<String, String>;
+
+#[derive(Debug)]
+pub struct Node {
+    pub children: Vec<Node>,
+    pub node_type: NodeType,
+}
+
+#[derive(Debug)]
+pub enum NodeType {
+    Text(String),
+    Element(ElementData),
+}
+
+#[derive(Debug)]
+pub struct ElementData {
+    pub tag_name: String,
+    pub attributes: AttrMap,
+}
+
+
+pub fn text(data: String) -> Node {
+    Node {
+        children: Vec::new(),
+        node_type: NodeType::Text(data),
+    }
+}
+
+pub fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children: children,
+        node_type: NodeType::Element(ElementData {
+            tag_name: name,
+            attributes: attrs,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod dom;
+
 fn main() {
     println!("Hello, world!");
 }
