7282ab71ace1bdc7da8341332a384f3ac826de09
arata <arata04962@gmail.com>
2020-05-22 00:45:33 +0900

    [add] DOM parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use crate::html::Parser;
+
 pub mod dom;
+pub mod html;
 
 fn main() {
-    println!("Hello, world!");
+    let source = r#"
+    <html>
+        <body>
+            <h1>Title</h1>
+            <div id="main" class="test">
+                <p>Hello <em>world</em>!</p>
+            </div>
+        </body>
+    </html>"#.to_string();
+    let nodes = Parser::parse(source);
+    println!("{:#?}", nodes);
 }
